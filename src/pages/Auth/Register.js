6d490dff2db28f04cd98394b724b2feb6e31ee72
Nandhini <nandhinivijaya03@gmail.com>
2023-12-13 10:12:29 +0530

    register changed

--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -17,7 +17,16 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/v1/auth/register", {
+      // const res = await axios.post("/api/v1/auth/register", {
+      //   name,
+      //   email,
+      //   password,
+      //   phone,
+      //   address,
+      //   answer,
+      // });
+
+      const res = await axios.post("https://ecommerce-backend-bqtg.onrender.com/api/v1/auth/register", {
         name,
         email,
         password,
@@ -25,6 +34,7 @@ const Register = () => {
         address,
         answer,
       });
+
       if (res && res.data.success) {
         toast.success(res.data && res.data.message);
         navigate("/login");
